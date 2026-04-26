2698d7ae142911ddd88cb9ba7b225c4ede4b1537
Malyshkin Sergey <pervaypocht@gmail.com>
2026-04-26 13:37:39 +0200

    Commit Test for VSC (first)

---
+++ Test FIle.py
@@ -0,0 +1,12 @@
+a = int(input("first number : "))
+b = int(input("second number : "))
+c = input("mathematical symbol : ")
+if c == "+":
+    print(a + b)
+elif c == "-":
+    print(a - b)
+elif c == "*":
+    print(a * b)
+elif c == "/":
+    print(a / b)
+print("hello world")
