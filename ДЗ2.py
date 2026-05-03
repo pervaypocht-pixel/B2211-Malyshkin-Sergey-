0020fdb39404fc36cc1e11507b793c59d04bae29
Malyshkin Sergey <pervaypocht@gmail.com>
2026-05-03 08:07:10 +0200

    ДЗ2

---
+++ ДЗ2.py
@@ -0,0 +1,5 @@
+first_name = input("Як тебе звуть?: ")
+last_name = input("Яке у тебе прізвище?: ")
+age = input("Скільки тобі років?: ")
+
+print("Привіт, мене звуть", first_name, last_name, "мені", age, "років")
