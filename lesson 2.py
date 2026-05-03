cc491ea8d268a4e55ce054c5aeccb22e4df0d9b5
Malyshkin Sergey <pervaypocht@gmail.com>
2026-05-03 13:14:41 +0200

    ООП (урок 2) теория ! запомнить !

--- lesson 2.py
+++ lesson 2.py
@@ -1,9 +1,24 @@
+"""
 ООП = обьектно орьенторованое програмирование.
 Парадігма(підход) = Проц. програм , ООП.
-Проц. програм. = набір фенкцій \ Алгоритмічна декомпозиція.
-ООП = Розбиття на сутності \ обьектна декомпозиція.
+Проц. програм. = набір фенкцій | Алгоритмічна декомпозиція.
+ООП = Розбиття на сутності | обьектна декомпозиція.
 
 Как ето работает (ООП) = классификация обьектов и присваивание им характеристик и возможномтей.
 Сутність = риба
 Обьект = Лосось
 Класс = шаблон по которому модно определить к какой сущности подходит обьект (поля класса и методы класса)
+"""
+class Dog:
+    def __init__(self, name, age, breed, weight):
+        self.name = name
+        self.age = age
+        self.breed = breed
+        self.weight = weight
+
+
+dog1 = Dog(name="Robert",age=6,breed="Golden Retriever",weight=30)
+dog2 = Dog(name="Edward",age=7,breed="Dachshund",weight=9.5)
+
+print(f"My dogs name is {dog1.name} , its {dog1.age} years old , its a {dog1.breed}, its weights about {dog1.weight} kg")
+print(f"His dogs name is {dog2.name} , its {dog2.age} years old , its a {dog2.breed}, its weights about {dog2.weight} kg")
