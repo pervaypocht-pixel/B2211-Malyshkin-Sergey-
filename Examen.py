ef99d21b54237515ae6ef2a964f48a09291881d9
Sergey Malyshkin <pervaypocht@gmail.com>
2026-05-31 12:01:25 +0200

    екзамен

--- Examen.py
+++ Examen.py
@@ -19,3 +19,17 @@ class CyberThreat(Threat):
 class PhysicalThreat(Threat):
     def escalate(self):
         self.level += 1
+
+
+class Scanner:
+    def analyze(self, text):
+
+        text = text.lower()
+
+        if "хакер" in text or "вирус" in text or "взлом" in text:
+            return CyberThreat(text, 4)
+
+        if "пожар" in text or "атака" in text or "дрон" in text:
+            return PhysicalThreat(text, 3)
+
+        return Threat(text, 2)
